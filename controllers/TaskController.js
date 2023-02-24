bd4ac583f976b749edb1b81c2d2b84be4e817594
britofelipe <arrudabritofelipe@gmail.com>
2023-02-24 17:05:29 -0300

    Saving data

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -2,10 +2,26 @@ const Task = require('../models/Task')
 
 module.exports = class TaskController {
 
-    static createTask(req, res) {
-        res.render('tasks/create')
-    }
     static showTasks(req, res) {
         res.render('tasks/all')
     }
+
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+    static async createTaskSave(req, res) {
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            priority: 1,
+            done: false
+        }
+
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+
 }
--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const TaskController = require('../controllers/TaskController')
 
-router.get('/add', TaskController.createTask)
 router.get('/', TaskController.showTasks)
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
 
 module.exports = router
