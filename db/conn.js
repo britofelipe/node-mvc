4055db4af45d82fb8b8f6aa33457018f5830f2d7
britofelipe <arrudabritofelipe@gmail.com>
2023-02-24 14:49:38 -0300

    Initial commit: setting configurations

---
+++ db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('nodemvc', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Connected successfully')
+} catch(error) {
+    console.log(`Connection not possible: ${error}`)
+}
+
+exports.default = sequelize
